b617332b84b4a03e720171e63a1efe01e0a2b744
Vikas-KC <vikas@LAPTOP-N34GTUSU>
2021-06-03 16:22:37 +0530

    first commit

---
+++ Jenkins_Demo/src/Demo1.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void test()
+	{
+//		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//		
+//		ChromeDriver driver = new ChromeDriver();
+//		
+//		driver.get("www.google.com");
+		System.out.println("test1");
+//		driver.close();
+	}
+}
---
+++ Jenkins_Demo/src/Demo2.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+	@Test
+	public void test()
+	{
+//		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//		
+//		ChromeDriver driver = new ChromeDriver();
+//		
+//		driver.get("www.google.com");
+		System.out.println("test2");
+//		driver.close();
+	}
+}
