ed7e886b6577ac36223b3be05cc0e3ad1331446c
Vikas-KC <vikas@LAPTOP-N34GTUSU>
2021-06-03 16:44:34 +0530

    Demo3 test script

---
+++ Jenkins_Demo/src/Demo3.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo3 {
+
+	@Test
+	public void test()
+	{
+
+		System.out.println("test3");
+	}
+}
